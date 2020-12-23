359d10ec6edb3caaa26507683b6fa02cd162cf31
Emmanoe DELAR <26841656+emmanoe@users.noreply.github.com>
2020-12-23 12:29:47 +0100

    String interpolation

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core'
 
 @Component({ // Creating the component, class decoration
     selector: 'courses', // selector css element
-    template: '<h2>Courses</h2>' 
+    template: '<h2>{{ getTitle() }}</h2>' // data binding
 })
 export class CoursesComponent {
+    title = "List of courses"
+
+    getTitle() {
+        return this.title
+    }
 }
